4fb737758920dc89120ac5b6683ea6b8e78841a4
Smriti7111 <smritibanjade71@gmail.com>
2021-04-27 10:48:15 +0545

    add birthday reminder project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+const App = () => {
+
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today </h3>
+        <List people={people} />
+        <button onClick={() => { setPeople([]) }}>Clear all</button>
+      </section>
+    </main >
+  );
+}
+
+export default App;
